55b3e2cc908d41ed99f2b6ed59eb17327cfcf347
thefudgeishot <hoethefudge@gmail.com>
2025-11-20 16:15:40 +0800

    networking features

---
+++ KidPaint2/src/MainWindow.java
@@ -0,0 +1,288 @@
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class MainWindow {
+    @FXML
+    ChoiceBox<String> chbMode;
+
+    @FXML
+    Canvas canvas;
+
+    @FXML
+    Pane container;
+
+    @FXML
+    Pane panePicker;
+
+    @FXML
+    Pane paneColor;
+
+    client client;
+    String username;
+    int numPixels = 50;
+    Stage stage;
+    AnimationTimer animationTimer;
+    int[][] data;
+    double pixelSize, padSize, startX, startY;
+    int selectedColorARGB;
+    boolean isPenMode = true;
+    LinkedList<Point> filledPixels = new LinkedList<Point>();
+
+    class Point{
+        int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public MainWindow(Stage stage, String username, client client) throws IOException {
+        this.username = username;
+        this.client = client;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainWindownUI.fxml"));
+        loader.setController(this);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        this.stage = stage;
+
+        stage.setScene(scene);
+        stage.setMinWidth(scene.getWidth());
+        stage.setMinHeight(scene.getHeight());
+
+        canvas.widthProperty().bind(container.widthProperty());
+        canvas.heightProperty().bind(container.heightProperty());
+        canvas.widthProperty().addListener(w->onCanvasSizeChange());
+        canvas.heightProperty().addListener(h->onCanvasSizeChange());
+
+        stage.setOnCloseRequest(event -> quit());
+
+        stage.show();
+        initial();
+
+        animationTimer.start();
+    }
+
+    /**
+     * Update canvas info when the window is resized
+     */
+    void onCanvasSizeChange() {
+        double w = canvas.getWidth();
+        double h = canvas.getHeight();
+        padSize = Math.min(w, h);
+        startX = (w - padSize)/2;
+        startY = (h - padSize)/2;
+        pixelSize = padSize / numPixels;
+    }
+
+    /**
+     * terminate this program
+     */
+    void quit() {
+        System.out.println("Bye bye");
+        stage.close();
+        System.exit(0);
+    }
+
+    /**
+     * Initialize UI components
+     * @throws IOException
+     */
+    void initial() throws IOException {
+        data = new int[numPixels][numPixels];
+
+        animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                render();
+            }
+        };
+
+        chbMode.setValue("Pen");
+
+        canvas.setOnMousePressed(event -> {
+            isPenMode = chbMode.getValue().equals("Pen");
+            filledPixels.clear();
+            if (isPenMode)
+                penToData(event.getX(), event.getY());
+        });
+
+        canvas.setOnMouseDragged(event -> {
+            if (isPenMode)
+                penToData(event.getX(), event.getY());
+        });
+
+        canvas.setOnMouseReleased(event->{
+            if (!isPenMode)
+                bucketToData(event.getX(), event.getY());
+        });
+
+        initColorMap();
+    }
+
+    /**
+     * Initialize color map
+     * @throws IOException
+     */
+    void initColorMap() throws IOException {
+        Image image = new Image("file:color_map.png");
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitHeight(30.0);
+        imageView.setPreserveRatio(true);
+        panePicker.getChildren().add(imageView);
+
+        double imageWidth = image.getWidth();
+        double imageHeight = image.getHeight();
+        double viewWidth = imageView.getBoundsInParent().getWidth();
+        double viewHeight = imageView.getBoundsInParent().getHeight();
+
+        double scaleX = imageWidth / viewWidth;
+        double scaleY = imageHeight / viewHeight;
+
+        pickColor(image, 0, 0, imageWidth, imageHeight);
+
+        panePicker.setOnMouseClicked(event -> {
+            double x = event.getX();
+            double y = event.getY();
+
+            int imgX = (int)(x * scaleX);
+            int imgY = (int)(y * scaleY);
+
+            pickColor(image, imgX, imgY, imageWidth, imageHeight);
+        });
+    }
+
+    /**
+     * Pick a color from the color map image
+     * @param image color map image
+     * @param imgX x position in the image
+     * @param imgY y position in the image
+     * @param imageWidth the width of the image
+     * @param imageHeight the height of the image
+     */
+    void pickColor(Image image, int imgX, int imgY, double imageWidth, double imageHeight) {
+        if (imgX >= 0 && imgX < imageWidth && imgY >= 0 && imgY < imageHeight) {
+            PixelReader reader = image.getPixelReader();
+
+            selectedColorARGB = reader.getArgb(imgX, imgY);
+
+            Color color = reader.getColor(imgX, imgY);
+            paneColor.setStyle("-fx-background-color:#" + color.toString().substring(2));
+        }
+    }
+
+    /**
+     * Invoked when the Pen mode is used. Update sketch data array and store updated pixels in a list named filledPixels
+     * @param mx mouse down/drag position x
+     * @param my mouse down/drag position y
+     */
+    void penToData(double mx, double my) {
+        if (mx > startX && mx < startX + padSize && my > startY && my < startY + padSize) {
+            int row = (int) ((my - startY) / pixelSize);
+            int col = (int) ((mx - startX) / pixelSize);
+            data[row][col] = selectedColorARGB;
+            filledPixels.add(new Point(col, row));
+        }
+    }
+
+    /**
+     * Invoked when the Bucket mode is used. It calls paintArea() to update sketch data array and store updated pixels in a list named filledPixels
+     * @param mx mouse down/drag position x
+     * @param my mouse down/drag position y
+     */
+    void bucketToData(double mx, double my) {
+        if (mx > startX && mx < startX + padSize && my > startY && my < startY + padSize) {
+            int row = (int) ((my - startY) / pixelSize);
+            int col = (int) ((mx - startX) / pixelSize);
+            paintArea(col, row);
+        }
+    }
+
+    /**
+     * Update the color of specific area
+     * @param col position of the sketch data array
+     * @param row position of the sketch data array
+     */
+    public void paintArea(int col, int row) {
+        int oriColor = data[row][col];
+        LinkedList<Point> buffer = new LinkedList<Point>();
+
+        if (oriColor != selectedColorARGB) {
+            buffer.add(new Point(col, row));
+
+            while(!buffer.isEmpty()) {
+                Point p = buffer.removeFirst();
+                col = p.x;
+                row = p.y;
+
+                if (data[row][col] != oriColor) continue;
+
+                data[row][col] = selectedColorARGB;
+                filledPixels.add(p);
+
+                if (col > 0 && data[row][col-1] == oriColor) buffer.add(new Point(col-1, row));
+                if (col < data[0].length - 1 && data[row][col+1] == oriColor) buffer.add(new Point(col+1, row));
+                if (row > 0 && data[row-1][col] == oriColor) buffer.add(new Point(col, row-1));
+                if (row < data.length - 1 && data[row+1][col] == oriColor) buffer.add(new Point(col, row+1));
+            }
+        }
+    }
+
+    /**
+     * Convert argb value from int format to JavaFX Color
+     * @param argb
+     * @return Color
+     */
+    Color fromARGB(int argb) {
+        return Color.rgb(
+                (argb >> 16) & 0xFF,
+                (argb >> 8) & 0xFF,
+                argb & 0xFF,
+                ((argb >> 24) & 0xFF) / 255.0
+        );
+    }
+
+
+    /**
+     * Render the sketch data to the canvas
+     */
+    void render() {
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        double x = startX;
+        double y = startY;
+
+        gc.setStroke(Color.GRAY);
+        for (int col = 0; col< numPixels; col++) {
+            for (int row = 0; row< numPixels; row++) {
+                gc.setFill(fromARGB(data[col][row]));
+                gc.fillOval(x, y, pixelSize, pixelSize);
+                gc.strokeOval(x, y, pixelSize, pixelSize);
+                x += pixelSize;
+            }
+            x = startX;
+            y += pixelSize;
+        }
+
+    }
+}
---
+++ KidPaint2/src/server.java
@@ -0,0 +1,160 @@
+import javax.xml.crypto.Data;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.TreeMap;
+
+public class server extends Thread {
+
+    class action { }
+
+    class client {
+        String name;
+        Socket socket;
+        public client(String name, Socket socket) {
+            this.name = name;
+            this.socket = socket;
+        }
+    }
+
+    class onBoarding extends Thread{
+        private ServerSocket serverSocket;
+        private ArrayList<client> clientList;
+        String groupName;
+
+        public onBoarding(ServerSocket serverSocket, ArrayList<client> clientList, String groupName) {
+            this.serverSocket = serverSocket;
+            this.clientList = clientList;
+            this.groupName = groupName;
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    Socket temp = this.serverSocket.accept();
+
+                    // identify incoming packet
+                    DataInputStream in = new DataInputStream(temp.getInputStream());
+                    DataOutputStream out = new DataOutputStream(temp.getOutputStream());
+
+                    switch (in.readInt()) {
+                        case 101:
+                            // onboarding
+                            String name = "";
+                            byte[] data = in.readNBytes(in.readInt());
+                            for (int i = 0; i != data.length; i++) {
+                                name += (char) data[i];
+                            }
+                            client newClient = new client(name,temp);
+                            synchronized (this.clientList) {
+                                this.clientList.add(newClient);
+                            }
+                            System.out.println("[server] onboarded player: " + name);
+                            out.writeInt(100);
+                            out.flush();
+                            break;
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    class advertiser extends Thread {
+        DatagramSocket server;
+        int port = 38878;
+        private byte[] message;
+
+        private byte[] buf = new byte[256];
+
+        public advertiser(int port,InetAddress addr, String name) throws SocketException {
+            server = new DatagramSocket(this.port);
+            message = (name + " " + addr.toString() + " " + port).getBytes();
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    DatagramPacket packet = new DatagramPacket(buf,buf.length);
+                    server.receive(packet);
+                    byte[] data = packet.getData();
+                    String temp = "";
+                    int index = 0;
+                    while (true) {
+                        if (data[index] == 0) { break; }
+                        temp += (char) data[index++];
+                    }
+                    if (temp.equals("*wave*")) {
+                        System.out.println("[server] broadcast received");
+                        InetAddress sender = packet.getAddress();
+                        int port = 38879;
+                        System.out.println("[server] sending advert to " + sender.toString() +":" + port);
+                        DatagramPacket advert = new DatagramPacket(message,message.length,sender,port);
+                        server.send(advert);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    int port = 38877;
+    ArrayList<client> clientList = new ArrayList<>();
+    String name;
+    ServerSocket serverSocket;
+
+    public server(String groupName, serverSharedData data) throws IOException {
+        // setup
+        serverSocket = new ServerSocket(port);
+        System.out.println("inet: " + serverSocket.getInetAddress().getHostAddress());
+        data.setData(serverSocket.getInetAddress(),port);
+        name = groupName;
+
+        Thread onboard = new Thread(new onBoarding(serverSocket,clientList,name));
+        Thread advertise = new Thread(new advertiser(port,serverSocket.getInetAddress(),name));
+        onboard.start();
+        advertise.start();
+
+        System.out.println("Server up...");
+        System.out.println("bound ports: \n-38877\n-38878");
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // registered users loop
+            synchronized (clientList) {
+                for (int i = 0; i != clientList.size(); i++) {
+                    try {
+                        client temp = clientList.get(i);
+                        DataInputStream in = new DataInputStream(temp.socket.getInputStream());
+                        DataOutputStream out = new DataOutputStream(temp.socket.getOutputStream());
+                        if (in.available() != 0) {
+                            // Actions
+                            switch (in.readInt()) {
+                                case 202:
+                                    // echo
+                                    System.out.println("[server] Action code 202");
+                                    String message = "echo";
+                                    out.writeInt(message.length());
+                                    out.writeBytes(message);
+                                    out.flush();
+                                    break;
+                            }
+
+                        }
+                    } catch (IOException e) {}
+                }
+            }
+
+        }
+    }
+}
